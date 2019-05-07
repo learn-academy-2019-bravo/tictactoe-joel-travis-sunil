acccc5a55e076087e7079742f47a3ea068383318
learn <learn@learn05.local>
2019-05-07 16:53:17 -0700

    faster alert

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -41,47 +41,48 @@ class Board extends Component {
     winning = (spacesIndex)=> {
         let {gameStatus, counter, spaces} = this.state
         if (spaces[0] === "x" && spaces[1] === "x" && spaces[2] === "x") {
-            alert(`${spaces[0]} is the winner`)
+
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[3] === "x" && spaces[4] === "x" && spaces[5] === "x") {
         // // gameStatus = "won"
-            alert("X wins")
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[6] === "x" && spaces[7] === "x" && spaces[8] === "x") {
             // // gameStatus = "won"
-            alert("X wins")
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[6] === "x" && spaces[7] === "x" && spaces[8] === "x"){
-            alert('X Wins')
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[0] === "x" && spaces[3] === "x" && spaces[6] === "x") {
-            alert('X wins')
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[1] === "x" && spaces[4] === "x" && spaces[7] === "x") {
-            alert('X wins')
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[2] === "x" && spaces[5] === "x" && spaces[8] === "x") {
-            alert('X wins')
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[0] === "x" && spaces[4] === "x" && spaces[8] === "x") {
-            alert('X wins')
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[6] === "x" && spaces[4] === "x" && spaces[2] === "x") {
-            alert('X wins')
+            setTimeout(function(){ alert("X Wins");}, 100)
         } else if (spaces[0] === "o" && spaces[1] === "o" && spaces[2] === "o") {
-            alert(`${spaces[0]} is the winner`)
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[3] === "o" && spaces[4] === "o" && spaces[5] === "o") {
         // // gameStatus = "won"
-            alert("O wins")
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[6] === "o" && spaces[7] === "o" && spaces[8] === "o") {
             // // gameStatus = "won"
-            alert("O wins")
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[6] === "o" && spaces[7] === "o" && spaces[8] === "o"){
-            alert('O Wins')
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[0] === "o" && spaces[3] === "o" && spaces[6] === "o") {
-            alert('O wins')
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[1] === "o" && spaces[4] === "o" && spaces[7] === "o") {
-            alert('O wins')
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[2] === "o" && spaces[5] === "o" && spaces[8] === "o") {
-            alert('O wins')
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[0] === "o" && spaces[4] === "o" && spaces[8] === "o") {
-            alert('O wins')
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (spaces[6] === "o" && spaces[4] === "o" && spaces[2] === "o") {
-            alert('O wins')
+            setTimeout(function(){ alert("O Wins");}, 100)
         } else if (counter === 8) {
-            alert("draw")
+            setTimeout(function(){ alert("No Winners for now");}, 100)
         }
     }
 
